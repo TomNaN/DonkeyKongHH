605ac74540ca07c8d80628cc72503f865adaf2f8
TomNaN <tomna.git@gmail.com>
2015-08-20 18:08:05 +0300

    FILEZS
    asdf

--- util.js
+++ util.js
@@ -1,13 +1,34 @@
 //This will be the home to some of the JS magic :)
-var c;
-var ctx;
+var canvas,
+    ctx,
+    tickrate,
+    xmlhttp = new XMLHttpRequest(),
+    listeners = [readFile],
+    text,
+    interval;
 
-function startup() {
-  c = document.getElementById("gameCanvas");
-  ctx = c.getContext("2d");
-  setInterval(game, 1000/60);  
+function begin() {
+  //listeners.forEach(e=>xmlhttp.removeEventListener("readystate",e));
+  xmlhttp.addEventListener("readystate",readFile);
+  xmlhttp.open("GET","resources/level.txt",false);
+  xmlhttp.send();
+  canvas = document.getElementById("gameCanvas");
+  ctx = canvas.getContext("2d");
+  canvas.width=640;
+  canvas.height=480;
+  interval = setInterval(game, 1000/60);  
 }
-
+function errorHandler(error){
+  clearInterval(interval);
+  throw error;
+}
 function game() {
-  console.log("Päivää päivää!");
+ // text?readMap(text):errorHandler("no map initialized");
+}
+function readMap(text){
+
+}
+function readFile(){
+  text = xmlhttp.readyState==4&&xmlhttp.status==4?xmlhttp.responseText:null;
+  console.log(xmlhttp.responseText);
 }
